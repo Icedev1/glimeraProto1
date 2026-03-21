e859f623d153223344e5af971a7f3613edf0321c
Sayfeddine Ben Aissa <90327486+Icedev1@users.noreply.github.com>
2026-03-21 18:55:17 +0100

    Npc sends signal when talked to.

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -1,19 +1,28 @@
 class_name NPC extends Node3D
 
+var is_player_in_range: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	Dialogic.signal_event.connect(DialogicSignal)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
+func DialogicSignal(arg:String):
+	#Prevents other npcs of the same type from listening to signal
+	if not is_player_in_range:
+		return
+	#starts battle(WIP)
+	if arg == "battle_start": 
+		print("battle started") 
+		
 func _on_area_3d_body_entered(body: Node3D) -> void:
+	is_player_in_range = true
 	Dialogue.interactRange.emit(self, true)
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
+	is_player_in_range = false
 	Dialogue.interactRange.emit(self, false)
