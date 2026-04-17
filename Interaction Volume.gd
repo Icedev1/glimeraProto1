9472ce78d982d2764b7b7da6959313d79f4245e8
Techmaster <52240882+Techmaster3000@users.noreply.github.com>
2026-04-17 19:16:11 +0200

    signing off

--- Interaction Volume.gd
+++ Interaction Volume.gd
@@ -18,7 +18,12 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node3D) -> void:
 	inRange = true
+	$CanvasLayer/Prompt.visible = true
+	var world_pos = global_transform.origin + Vector3(0, 0.5, 0) # offset above
+	var screen_pos = get_viewport().get_camera_3d().unproject_position(world_pos)
+	$CanvasLayer/Prompt.position = screen_pos
 
 
 func _on_body_exited(body: Node3D) -> void:
 	inRange = false
+	$CanvasLayer/Prompt.visible = false
