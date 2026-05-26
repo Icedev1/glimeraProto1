c0207c8fc62f59092ad87ae0601dc9ff6d96e196
Oliver Henke <124154618+clewzz@users.noreply.github.com>
2026-05-26 13:12:32 +0200

    autoload/continue works
    You can now click continue from the menu and it will load the most recent played save(when you switch scene)

--- scripts/street_exit.gd
+++ scripts/street_exit.gd
@@ -1,11 +1,11 @@
 extends Area3D
-
 @export var target_street : String
 @export var spawn_name : String
-
 func _on_body_entered(body: Node3D) -> void:
 	if body is CharacterBody3D:
-
+		StoryFlags.current_scene = target_street
+		StoryFlags.checkpoint_position = body.global_position
+		SaveManager.save(0)
+		print("Saved! Scene: ", StoryFlags.current_scene, " Pos: ", StoryFlags.checkpoint_position)
 		var game = get_tree().current_scene
-
 		game.transition_to_street(target_street, spawn_name)
