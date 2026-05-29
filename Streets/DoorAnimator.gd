13c3b1d220510749af4b98144f39f8de0ba25830
Techmaster <52240882+Techmaster3000@users.noreply.github.com>
2026-05-29 16:22:57 +0200

    gates are openable through code.

---
+++ Streets/DoorAnimator.gd
@@ -0,0 +1,17 @@
+extends AnimationPlayer
+
+@export var currentScene: String
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	StoryFlags.OpenGate.connect(gateOpen)
+	if StoryFlags.Layer1GateUnlock and currentScene == "1-2":
+		gateOpen()
+	elif StoryFlags.Layer2GateUnlock and currentScene == "2-1":
+		gateOpen()
+
+func gateOpen():
+	play("gate_opening")
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/Globals/story_flags.gd
+++ scripts/Globals/story_flags.gd
@@ -3,6 +3,8 @@ extends Node
 #Enemy Flags
 var Enemy1_2Defeat = false
 var EnemyM_2Defeat = false
+var Layer1GateUnlock = false
+var Layer2GateUnlock = false
 
 
 #Item Flags
@@ -10,3 +12,5 @@ var violinObtained : bool = false
 
 #Quest Flags
 var statueQuestComplete : bool = false
+
+signal OpenGate
