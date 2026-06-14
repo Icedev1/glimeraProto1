53121eda0d96a45bb8a72b5f131752fe8320861b
Sayfeddine Ben Aissa <90327486+Icedev1@users.noreply.github.com>
2026-06-14 12:43:54 +0200

    audio fix
    fixed audio

--- ui/audio_control.gd
+++ ui/audio_control.gd
@@ -6,9 +6,17 @@ var audio_bus_id
 func _ready() -> void:
 	audio_bus_id = AudioServer.get_bus_index(audio_bus_name)
 	#sets sound
-	AudioServer.set_bus_volume_db(audio_bus_id, 0.5)
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 1.0)
-
+	if AudioServer.get_bus_volume_db(audio_bus_id) == 1:
+		AudioServer.set_bus_volume_db(audio_bus_id, 0.5)
+	else:
+		AudioServer.set_bus_volume_db(audio_bus_id, AudioServer.get_bus_volume_db(audio_bus_id))
+		
+	if AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")) == 1:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 1.0)
+	else:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
+	
+	value = db_to_linear(AudioServer.get_bus_volume_db(audio_bus_id))
 
 func _on_value_changed(value: float) -> void:
 	var db = linear_to_db(value)
